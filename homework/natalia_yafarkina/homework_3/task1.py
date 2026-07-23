82db0f5d2491e62ba67836eda2f8fd5fba5af005
Natalia Yafarkina <yafarkina@gmail.com>
2026-07-23 21:57:27 +0400

    добавлены задания по теме переменные и типы

---
+++ homework/natalia_yafarkina/homework_3/task1.py
@@ -0,0 +1,10 @@
+# Даны 2 числа a и b. Получить их сумму, разность и произведение
+
+a=5
+b=8
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+print(addition)
+print(subtraction)
+print(multiplication)
