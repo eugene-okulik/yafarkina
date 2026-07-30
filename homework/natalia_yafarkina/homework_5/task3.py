34d50fa002a65c2aef70f668031de5c53019d263
Natalia Yafarkina <yafarkina@gmail.com>
2026-07-30 12:13:18 +0400

    добавлено домашнее задание №5

---
+++ homework/natalia_yafarkina/homework_5/task3.py
@@ -0,0 +1,4 @@
+students = ', '.join(['Ivanov', 'Petrov', 'Sidorov'])
+subjects = ', '.join(['math', 'biology', 'geography'])
+result_text = 'Students {0} study these subjects: {1}'.format(students, subjects)
+print(result_text)
