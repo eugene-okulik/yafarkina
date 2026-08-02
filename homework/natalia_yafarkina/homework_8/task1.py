f87454fd0a6bc2d3131891dfe950661964bce8ee
Natalia Yafarkina <yafarkina@gmail.com>
2026-08-03 00:11:45 +0400

    исправлены ошибки код стайла

--- homework/natalia_yafarkina/homework_8/task1.py
+++ homework/natalia_yafarkina/homework_8/task1.py
@@ -2,8 +2,8 @@ import random as rnd
 
 
 salary = int(input('введите число'))
-bonus = bool(rnd.randint(0,1))
+bonus = bool(rnd.randint(0, 1))
 if bonus:
-    print(f"{salary}, {bonus} - ${salary + rnd.randrange(1,1001)}")
+    print(f"{salary}, {bonus} - ${salary + rnd.randrange(1, 1001)}")
 else:
     print(f"{salary}, {bonus} - ${salary}")
--- homework/natalia_yafarkina/homework_8/task2.py
+++ homework/natalia_yafarkina/homework_8/task2.py
@@ -8,10 +8,9 @@ def fibonacci():
 lst = [5, 200, 1000, 10000, 100000]
 for n in lst:
     fib = fibonacci()
-    print(f'ряд Фибоначи для {n}:',  end=' ')
+    print(f'ряд Фибоначи для {n}:', end=' ')
     for num in fib:
         if num > n:
             break
         print(num, end=' ')
     print()
-
