f1e38833ba2546facd34a93f90160d3b1beec1b1
Natalia Yafarkina <yafarkina@gmail.com>
2026-08-02 23:52:40 +0400

    добавлено домашнее задание 8

---
+++ homework/natalia_yafarkina/homework_8/task2.py
@@ -0,0 +1,17 @@
+def fibonacci():
+    a = 0
+    b = 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+lst = [5, 200, 1000, 10000, 100000]
+for n in lst:
+    fib = fibonacci()
+    print(f'ряд Фибоначи для {n}:',  end=' ')
+    for num in fib:
+        if num > n:
+            break
+        print(num, end=' ')
+    print()
+
