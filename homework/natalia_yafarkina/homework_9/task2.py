09fc15fec3049c6134cebadc400115c42f622e75
Natalia Yafarkina <yafarkina@gmail.com>
2026-08-05 18:15:34 +0400

    исправлены ошибки код стайла

--- homework/natalia_yafarkina/homework_9/task2.py
+++ homework/natalia_yafarkina/homework_9/task2.py
@@ -1,4 +1,5 @@
-temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
+temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30,
+                29, 25, 27, 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 hot_temperatures = list(filter(lambda x: x > 28, temperatures))
 avg_hot_temperatures = (sum(hot_temperatures) / len(hot_temperatures))
 max_hot_temperatures = max(hot_temperatures)
