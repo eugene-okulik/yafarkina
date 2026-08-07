62823fb6f2f1b7ff7d42a6fb2c84a20c43b47680
Natalia Yafarkina <yafarkina@gmail.com>
2026-08-07 12:39:07 +0400

    исправлены ошибки код стайла

--- homework/natalia_yafarkina/homework_10/task4.py
+++ homework/natalia_yafarkina/homework_10/task4.py
@@ -19,23 +19,15 @@ print('вариант 1: ', price_dict1)
 # вариант 2
 split_list2 = PRICE_LIST.split()
 key2 = [split_list2[i] for i in range(0, len(split_list2), 2)]
-values2 = [split_list2[i+1].replace('р', '') for i in range(0, len(split_list2), 2)]
+values2 = [split_list2[i + 1].replace('р', '') for i in range(0, len(split_list2), 2)]
 d_values2 = list(map(int, values2))
-price_dict2 = {key : values for key, values in zip(key2, d_values2)}
-print('вариант 2: ',price_dict2)
+price_dict2 = {key: values for key, values in zip(key2, d_values2)}
+print('вариант 2: ', price_dict2)
 
 
 # вариант 3
 split_list3 = PRICE_LIST.split()
 clear_list3 = [item.rstrip('р') for item in split_list3]
 new_list = [int(item) if item.isdigit() else item for item in clear_list3]
-price_dict3 = {new_list[i] : new_list[i+1] for i in range(0, len(new_list), 2)}
+price_dict3 = {new_list[i]: new_list[i + 1] for i in range(0, len(new_list), 2)}
 print('вариант 3: ', price_dict3)
-
-
-
-
-
-
-
-
